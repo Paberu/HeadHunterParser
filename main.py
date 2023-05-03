0151ba04b3a09d077a0d6139d723859a57c7d80c
Paberu <paberu.tocador@gmail.com>
2023-05-03 06:26:29 +0300

    I've got all posibilities of main tag of interesting part of page.

--- main.py
+++ main.py
@@ -33,34 +33,54 @@ def get_vacancies_test():
     return vacancies_ids
 
 
-def get_requirements(id):
+def get_vacancy_user_part(id):
     path = f'https://pskov.hh.ru/vacancy/{id}'
     r = requests.get(path, headers={'User-Agent': 'Custom'})
     soup = BeautifulSoup(r.text, 'html.parser')
-    vacancy_details = soup.find('div', class_='g-user-content')
+    vacancy_details = soup.find('div', class_="tmpl_hh_wrapper")
     if not vacancy_details:
-        return ()
-    details_list = [detail.text for detail in vacancy_details.findAll('li')]
+        vacancy_details = soup.find('div', attrs={'data_qa': 'vacancy_description'})
+    if not vacancy_details:
+        vacancy_details = soup.find('div', class_='g-user-content')
+    return vacancy_details
+
+
+def get_requirements_from_page_part(page_part):
+    details_list = [detail.text for detail in page_part.findAll('li')]
+    list_points = ('•', '-', '—')
     if not details_list:
-        details_list = [detail.text for detail in vacancy_details.findAll('p') if detail.text.startswith('•')]
-    # if not details_list:
-    #     print(id)
-    return (details_list, )
+        details_list = [detail.text for detail in page_part.findAll('p') if detail.text.startswith(list_points)]
+    if not details_list:
+        # print(page_part)
+        return []
+    return details_list
 
-
-def save_to_file():
-    pass
 
 if __name__ == '__main__':
 
     worked_vacancies = {}
     failed_vacancies = []
     vacancies_ids = get_vacancies()
+    vacancies_user_parts = {}
+    requirements = {}
+    tmp_file = open('crazy_list.html', 'w', errors='replace')
     for vacancy_id in vacancies_ids:
-        vacancy_requirements = get_requirements(vacancy_id)
-        if vacancy_requirements:
-            worked_vacancies[vacancy_id] = vacancy_requirements
-        else:
-            failed_vacancies.append(vacancy_id)
-    print(f'На {len(worked_vacancies)} вакансий с нормальным форматированием пришлось {len(failed_vacancies)} без '
-          f'нормального форматирования.')
+        vacancy_user_part = get_vacancy_user_part(vacancy_id)
+        requirement = get_requirements_from_page_part(vacancy_user_part)
+        vacancies_user_parts[vacancy_id] = vacancy_user_part
+        requirements[vacancy_id] = requirement
+        print(f'<h2>{vacancy_id}</h2>', file=tmp_file)
+        print(vacancy_user_part, file=tmp_file)
+    tmp_file.close()
+    #     if vacancy_user_part:
+    #         worked_vacancies[vacancy_id] = vacancy_user_part
+    #     else:
+    #         failed_vacancies.append(vacancy_id)
+    # print(f'На {len(worked_vacancies)} вакансий с нормальным форматированием пришлось {len(failed_vacancies)} без '
+    #       f'нормального форматирования.')
+    # print('Без форматирования: ', failed_vacancies)
+    #
+    # with open('crazy_list.html', 'w') as temp_file:
+    #     for key, value in vacancies_user_parts.items():
+    #         temp_file.write(str(key))
+    #         temp_file.write(str(value, errors='replace'))
