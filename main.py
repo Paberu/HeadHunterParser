0ee1f9c8e7373273f8f6a1d55a83b238e012037c
Paberu <paberu.tocador@gmail.com>
2023-05-05 06:25:19 +0300

    Renamed faster version of get_vacancies_id(). Checked all the types of the main part of the page. Parsed that main part and got rid of the tags (when page type is "tmpl_hh_wrapper".

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import re
-from pprint import pprint
 
 import requests
 from bs4 import BeautifulSoup
-import pandas
 
 
-def get_vacancies():
+def get_vacancies_ids():
     path = 'https://hh.ru/search/vacancy'
-    search = {'text': 'Python'}
+    search = {'text': 'Python', 'schedule': 'remote', 'salary': '90000'}
     r = requests.get(path, headers={'User-Agent': 'Custom'}, params=search)
     hh_response = r.text
     pre_url_regex = re.compile(r'm.hh.ru/vacancy/(\d+)"},')
@@ -23,9 +21,9 @@ def get_vacancies():
     return vacancies_ids
 
 
-def get_vacancies_test():
+def get_first_50_vacancies_ids():
     path = 'https://hh.ru/search/vacancy'
-    search = {'text': 'Python'}
+    search = {'text': 'Python', 'schedule': 'remote', 'salary': '90000'}
     r = requests.get(path, headers={'User-Agent': 'Custom'}, params=search)
     hh_response = r.text
     pre_url_regex = re.compile(r'm.hh.ru/vacancy/(\d+)"},')
@@ -33,11 +31,11 @@ def get_vacancies_test():
     return vacancies_ids
 
 
-def get_vacancy_user_part(id):
+def get_looked_for_page_part(id):
     path = f'https://pskov.hh.ru/vacancy/{id}'
     r = requests.get(path, headers={'User-Agent': 'Custom'})
     soup = BeautifulSoup(r.text, 'html.parser')
-    vacancy_details = soup.find('div', class_="tmpl_hh_wrapper")
+    vacancy_details = soup.find('div', class_='tmpl_hh_wrapper')
     if not vacancy_details:
         vacancy_details = soup.find('div', attrs={'data_qa': 'vacancy_description'})
     if not vacancy_details:
@@ -45,42 +43,52 @@ def get_vacancy_user_part(id):
     return vacancy_details
 
 
-def get_requirements_from_page_part(page_part):
-    details_list = [detail.text for detail in page_part.findAll('li')]
-    list_points = ('•', '-', '—')
-    if not details_list:
-        details_list = [detail.text for detail in page_part.findAll('p') if detail.text.startswith(list_points)]
-    if not details_list:
-        # print(page_part)
-        return []
-    return details_list
+def parse_looked_for_page_part(page_part):
+    user_content = page_part.find('div', class_='vacancy-branded-user-content')
+    if not user_content:
+        user_content = page_part
+    current_key = BeautifulSoup('<strong></strong>', 'html.parser').strong  # костыль для создания ключа с пустой строкой и тэгом strong
+    user_content_keys = []
+    user_content_parts = {}
+    for tag in user_content.descendants:
+        if tag.name == 'strong':
+            user_content_keys.append(tag)
+            current_key = tag
+        elif tag.name == 'ul':
+            user_content_parts[current_key] = tag
+    if len(user_content_keys) != len(user_content_parts.keys()):
+        for key in user_content_keys:
+            if key not in user_content_parts.keys():
+                user_content_parts[key] = key.parent
+    return user_content_parts
+
+
+def detag(parts):
+    detagged_parts = {}
+    for key, value in parts.items():
+        if value.name == 'ul':
+            detagged_parts[key.string] = [detail.text for detail in value.findAll('li')]
+        else:
+            detagged_parts[key.string] = value.string
+    return detagged_parts
 
 
 if __name__ == '__main__':
-
-    worked_vacancies = {}
-    failed_vacancies = []
-    vacancies_ids = get_vacancies()
+    vacancies_ids = get_first_50_vacancies_ids()
     vacancies_user_parts = {}
     requirements = {}
     tmp_file = open('crazy_list.html', 'w', errors='replace')
+    tmplt_hh_file = open('tmplt_hh.html', 'w', errors='replace')
     for vacancy_id in vacancies_ids:
-        vacancy_user_part = get_vacancy_user_part(vacancy_id)
-        requirement = get_requirements_from_page_part(vacancy_user_part)
+        vacancy_user_part = get_looked_for_page_part(vacancy_id)
+        # requirement = parse_looked_for_page_part(vacancy_user_part)
         vacancies_user_parts[vacancy_id] = vacancy_user_part
-        requirements[vacancy_id] = requirement
+        # requirements[vacancy_id] = requirement
         print(f'<h2>{vacancy_id}</h2>', file=tmp_file)
         print(vacancy_user_part, file=tmp_file)
+        if vacancy_user_part.attrs['class'] == ['tmpl_hh_wrapper']:
+            print(f'<h2>{vacancy_id}</h2>', file=tmplt_hh_file)
+            print(vacancy_user_part, file=tmplt_hh_file)
+            print(detag(parse_looked_for_page_part(vacancy_user_part)))
     tmp_file.close()
-    #     if vacancy_user_part:
-    #         worked_vacancies[vacancy_id] = vacancy_user_part
-    #     else:
-    #         failed_vacancies.append(vacancy_id)
-    # print(f'На {len(worked_vacancies)} вакансий с нормальным форматированием пришлось {len(failed_vacancies)} без '
-    #       f'нормального форматирования.')
-    # print('Без форматирования: ', failed_vacancies)
-    #
-    # with open('crazy_list.html', 'w') as temp_file:
-    #     for key, value in vacancies_user_parts.items():
-    #         temp_file.write(str(key))
-    #         temp_file.write(str(value, errors='replace'))
+    tmplt_hh_file.close()
