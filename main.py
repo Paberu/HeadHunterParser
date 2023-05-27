8cc32edea0b1e6ac7030c74417b30e409f23ec32
Paberu <paberu.tocador@gmail.com>
2023-05-27 10:53:28 +0300

    Doing it with a GUI, some new experience.
    Added some filters for vacancies. Still learning what to do with DearPyGUI.

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ if __name__ == '__main__':
 
     main_data = {
         'hh_parser': None,
-        'vacancies': {}
+        'vacancies': {},
+        'key_skills': {},
+        'salaries': {},
+        'experience': {}
     }
 
     def create_hhparser(sender, data, user_data):
@@ -23,11 +26,16 @@ if __name__ == '__main__':
         else:
             schedule = ''
         hh_parser = HHParser(search, schedule)
+        hh_parser.load_vacancies_from_json()
         hh_parser.get_vacancies()
         main_data['hh_parser'] = hh_parser
         for vacancy in hh_parser.vacancies:
             main_data['vacancies'][vacancy.id] = vacancy.title
+        main_data['key_skills'] = hh_parser.key_skills
         dpg.configure_item(vacancies_list, items=list(main_data['vacancies'].values()))
+        dpg.configure_item(salaries_list, items=list(main_data['salaries'].values()))
+        dpg.configure_item(key_skills_list, items=list(main_data['key_skills'].values()))
+        dpg.configure_item(experience_list, items=list(main_data['experience'].values()))
 
     dpg.create_context()
     with dpg.font_registry():
@@ -35,19 +43,27 @@ if __name__ == '__main__':
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
     dpg.bind_font('default_font')
 
-    with dpg.window(label='Begin search', height=130, width=500):
-        dpg.add_text('Decide what to look for:')
-        search = dpg.add_input_text(label='Print the words for vacancies to search: ', default_value='Python',
+    with dpg.window(label='Настройка поиска', height=130, width=500):
+        dpg.add_text('Введите, что будем искать:')
+        search = dpg.add_input_text(label='Введите слова, по которым будет вестись поиск.', default_value='Python',
                                     width=150)
-        remote = dpg.add_checkbox(label='Remote job is the only option.', default_value=True)
-        dpg.add_button(label='Start searching', callback=create_hhparser, user_data=[search, remote])
+        remote = dpg.add_checkbox(label='Только удаленная работа?', default_value=True)
+        dpg.add_button(label='Начать поиск', callback=create_hhparser, user_data=[search, remote])
 
-    with dpg.window(label='Vacancies', height=300, width=500, pos=[0, 130]):
+    with dpg.window(label='Вакансии', height=300, width=500, pos=[0, 130]):
         vacancies_list = dpg.add_listbox(items=list(main_data['vacancies'].values()))
 
-    dpg.create_viewport(title='HHParser', width=505, min_height=430, max_height=800)
+    with dpg.window(label='Выберите диапазон зарплат.', height=300, width=500, pos=[500, 0]):
+        salaries_list = dpg.add_listbox(items=list(main_data['salaries'].values()))
+
+    with dpg.window(label='Выбрать ключевые навыки.', height=300, width=500, pos=[500, 300]):
+        key_skills_list = dpg.add_listbox(items=list(main_data['key_skills'].values()))
+
+    with dpg.window(label='Выбрать стаж.', height=300, width=500, pos=[500, 600]):
+        experience_list = dpg.add_listbox(items=list(main_data['experience'].values()))
+
+    dpg.create_viewport(title='HHParser', width=505, min_height=1030, max_height=800)
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
-
     dpg.destroy_context()
