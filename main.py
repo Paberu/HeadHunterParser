39a3bffb7e9b3c3c8ac28819b24eb73372ee0cd9
Paberu <paberu.tocador@gmail.com>
2023-06-07 07:00:56 +0300

    Still tuning the filtering: choosing by experience is not taking vacancies from the whole spectrum but only from the list filtered by salary filter.
    Also: made unnecessary widgets not visible until they're needed.

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ from Vacancy import Vacancy
 if __name__ == '__main__':
 
     FONT_SIZE = 14
-    WIDGET_HEIGHT = (FONT_SIZE + 1) * 10
+    SEARCH_WIDGET_HEIGHT = (FONT_SIZE + 1) * 10 + 10
+    FILTER_WIDGET_HEIGHT = (FONT_SIZE + 1) * 25 + 10
+    VACANCIES_WIDGET_HEIGHT = SEARCH_WIDGET_HEIGHT + FILTER_WIDGET_HEIGHT
     SMALL_WIDGET_HEIGHT = (FONT_SIZE + 1) * 4
     WIDGET_WIDTH = 500
 
     main_data = {
         'hh_parser': None,
-        'vacancies': {},
+        'vacancies': [],
         'key_skills': {},
         'salaries': [],
         'experience': {}
@@ -41,13 +43,13 @@ if __name__ == '__main__':
         # hh_parser.save_vacancies_to_json()
 
         main_data['hh_parser'] = hh_parser
-        for vacancy in hh_parser.vacancies:
-            main_data['vacancies'][vacancy.id] = vacancy.title
+        main_data['vacancies'] = hh_parser.vacancies
         main_data['key_skills'] = hh_parser.sort_key_skills()
         main_data['salaries'] = hh_parser.split_vacancies_by_salary()
         main_data['experience'] = hh_parser.experience
-        dpg.configure_item(vacancies_list, items=list(main_data['vacancies'].values()))
-        dpg.configure_item(salaries_list, items=list(main_data['salaries']))
+        dpg.configure_item(vacancies_list, items=list(main_data['vacancies']))
+        dpg.configure_item('salary_label', show=True)
+        dpg.configure_item(salaries_list, items=list(main_data['salaries']), show=True)
         dpg.configure_item(key_skills_list, items=list(main_data['key_skills']))
         dpg.configure_item(experience_list, items=list(main_data['experience']))
 
@@ -68,21 +70,29 @@ if __name__ == '__main__':
             lowest_salary = highest_salary - 40000
 
         filtered_vacancies = []
+        filtered_vacancies_experience = {'не требуется': 0, '1–3 года': 0, '3–6 лет': 0, 'более 6 лет': 0}
         for vacancy in main_data['hh_parser'].vacancies:
             if salary_position != 0:
                 for salary_part in vacancy.salary:
                     if lowest_salary < salary_part < highest_salary:
-                        filtered_vacancies.append(vacancy)
-                        continue
+                        if vacancy not in filtered_vacancies:
+                            filtered_vacancies.append(vacancy)
+                            filtered_vacancies_experience[vacancy.experience] += 1
+                            continue
             else:
                 if not vacancy.salary:
-                    filtered_vacancies.append(vacancy)
+                    if vacancy not in filtered_vacancies:
+                        filtered_vacancies.append(vacancy)
+                        filtered_vacancies_experience[vacancy.experience] += 1
 
-        dpg.configure_item(vacancies_list, items=filtered_vacancies)
+        main_data['filtered_vacancies'] = filtered_vacancies
+        dpg.configure_item(vacancies_list, items=filtered_vacancies, show=True)
+        dpg.configure_item(experience_list, items=list(filtered_vacancies_experience), show=True)
+        dpg.configure_item('experience_label', show=True)
 
     def filter_by_experience(sender, data):
         filtered_vacancies = []
-        for vacancy in main_data['hh_parser'].vacancies:
+        for vacancy in main_data['filtered_vacancies']:
             if vacancy.experience == data:
                 filtered_vacancies.append(vacancy)
         dpg.configure_item(vacancies_list, items=filtered_vacancies)
@@ -97,45 +107,42 @@ if __name__ == '__main__':
 
     dpg.create_context()
 
-    # # getting the window sizes
-    # main_width = dpg.get_item_width("Main")
-    # main_height = dpg.get_item_height("Main")
-    # print(f'{main_width}x{main_height}')
-    #
     with dpg.font_registry():
         with dpg.font(r'C:\Windows\Fonts\Arial.ttf', FONT_SIZE, default_font=True, id='default_font'):
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
     dpg.bind_font('default_font')
 
-    with dpg.window(label='Настройка поиска', height=WIDGET_HEIGHT, width=WIDGET_WIDTH):
+    with dpg.window(label='Настройка поиска', height=SEARCH_WIDGET_HEIGHT, width=WIDGET_WIDTH):
         dpg.add_text('Введите слова, по которым будет вестись поиск:')
         search = dpg.add_input_text(default_value='Python', width=WIDGET_WIDTH)
         remote = dpg.add_checkbox(label='Только удаленная работа?', default_value=True)
         dpg.add_text('Введите границы интересующего диапазона зарплат:')
-        # bottom_of_salary_range = dpg.add_input_int()
-        # top_of_the_salary_range = dpg.add_input_int()
         dpg.add_button(label='Начать поиск', callback=create_hhparser, user_data=[search, remote])
 
-    with dpg.window(label='Вакансии', height=2*WIDGET_HEIGHT, width=WIDGET_WIDTH, pos=[WIDGET_WIDTH, 0]):
-        vacancies_list = dpg.add_listbox(items=list(main_data['vacancies'].values()),
-                                         num_items=10, width=WIDGET_WIDTH)
-
-    with dpg.window(label='Выберите диапазон зарплат.', height=WIDGET_HEIGHT, width=WIDGET_WIDTH, pos=[0, WIDGET_HEIGHT]):
+    with dpg.window(label='Фильтр вакансий', height=FILTER_WIDGET_HEIGHT, width=WIDGET_WIDTH,
+                    pos=[0, SEARCH_WIDGET_HEIGHT]):
+        dpg.add_text('Выберите диапазон зарплат:', show=False, tag='salary_label')
         salaries_list = dpg.add_listbox(items=list(main_data['salaries']),
                                         num_items=10, width=WIDGET_WIDTH,
-                                        callback=filter_by_salary)
-
-    with dpg.window(label='Выбрать стаж.', height=SMALL_WIDGET_HEIGHT, width=WIDGET_WIDTH, pos=[0, WIDGET_HEIGHT*2]):
+                                        callback=filter_by_salary,
+                                        show=False)
+        dpg.add_text('Выберите желаемый опыт работы:', show=False, tag='experience_label')
         experience_list = dpg.add_listbox(items=list(main_data['experience'].values()),
                                           num_items=5, width=WIDGET_WIDTH,
-                                          callback=filter_by_experience)
-
-    with dpg.window(label='Выбрать ключевые навыки.', height=WIDGET_HEIGHT, width=WIDGET_WIDTH, pos=[0, WIDGET_HEIGHT*2+SMALL_WIDGET_HEIGHT]):
+                                          callback=filter_by_experience,
+                                          show=False)
+        dpg.add_text('Выберите ключевой навык из описания вакансии:', show=False, tag='key_skills_label')
         key_skills_list = dpg.add_listbox(items=list(main_data['key_skills']),
                                           num_items=10, width=WIDGET_WIDTH,
-                                          callback=filter_by_key_skills)
+                                          callback=filter_by_key_skills,
+                                          show=False)
 
-    dpg.create_viewport(title='HHParser', width=1005, min_height=800, max_height=1000)
+    with dpg.window(label='Вакансии', height=VACANCIES_WIDGET_HEIGHT, width=WIDGET_WIDTH, pos=[WIDGET_WIDTH, 0]):
+        vacancies_list = dpg.add_listbox(items=list(main_data['vacancies']),
+                                         num_items=25, width=WIDGET_WIDTH,
+                                         show=False)
+
+    dpg.create_viewport(title='HHParser')
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
