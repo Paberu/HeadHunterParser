7f274a9b77a4937412ccd0a3b9fec08a39810f80
Paberu <paberu.tocador@gmail.com>
2024-05-03 22:06:15 +0300

    Вынес весь статичный функционал в новый класс VacancyParser

--- Vacancy.py
+++ Vacancy.py
@@ -1,21 +1,10 @@
-import re
 import requests
-from bs4 import BeautifulSoup, NavigableString, Tag
+from bs4 import BeautifulSoup
 
-# для валют переписать parse_salary на фабрику функций или вроде того
-TAXES = 0.13
-COURSES = {
-    'USD': 94.15,
-    'KZT': 0.21,
-    'руб': 1,
-    '₽': 1,
-    '$': 94.15,
-    '€': 100.08,
-    '': 1
-}
+from VacancyParser import VacancyParser
 
 
-class Vacancy:
+class Vacancy(VacancyParser):
 
     def __init__(self, id, title, salary, experience, detailed_information, key_skills):
         self.id = id
@@ -64,55 +53,3 @@ class Vacancy:
         vacancy = cls(id=id, title=title, salary=salary, experience=experience,
                       detailed_information=detailed_information, key_skills=key_skills)
         return vacancy
-
-    @staticmethod
-    def clearify(soup):
-        for tag in soup.find_all():
-            if len(tag.get_text(strip=True)) == 0:
-                tag.replace_with(NavigableString(''))
-        return soup
-
-    @staticmethod
-    def parse_title(soup):
-        return ''.join(soup.find('h1', attrs={'data-qa': 'vacancy-title'}).stripped_strings)
-
-    @staticmethod
-    def parse_salary(soup):
-        salary_tag = soup.find('div', attrs={'data-qa': 'vacancy-salary'})
-        if not salary_tag:
-            return []
-        salary = ''.join(salary_tag.find('span').stripped_strings)
-        salary = salary.replace('\xa0', '')  # заменяем разделитель разряда пустой строкой
-        currency_template = r'(' + '|'.join(COURSES.keys()) + ')(.*)'  # собираем шаблон из всех значимых валют
-        value_template = re.compile(currency_template)  # компилируем шаблон поиска
-        try:
-            currency, tax_flag = value_template.findall(salary)[0]  # выполняем поиск, группируем результат
-        except IndexError:
-            return []
-
-        money_template = re.compile(r'(\d{3,7})')
-        salary_delta = list(map(int, money_template.findall(salary))) # переводим все найденные значения в int, формируем список
-        for i in range(len(salary_delta)):  # нельзя воспользоваться итератором, надо отредактировать каждое значение в массиве
-            if tax_flag == 'до вычета налогов':  # высчитываем налог, чтобы не тешить себя иллюзиями
-                salary_delta[i] = round(salary_delta[i] * (1 - TAXES))
-            salary_delta[i] = salary_delta[i] * COURSES[currency]
-        return salary_delta
-
-    @staticmethod
-    def parse_experience(soup):
-        return ' '.join(soup.find('span', attrs={'data-qa': 'vacancy-experience'}).stripped_strings)
-
-    @staticmethod
-    def parse_key_skills(soup):
-        key_skills_block = soup.find('div', class_='bloko-tag-list')
-        key_skills = [str(key_skill.string) for key_skill in key_skills_block.find_all('span')] if key_skills_block else []
-        return key_skills
-
-    @staticmethod
-    def parse_detailed_information(soup):
-        vacancy_details = soup.find('div', class_='vacancy-branded-user-content')
-        if not vacancy_details:
-            vacancy_details = soup.find('div', attrs={'data_qa': 'vacancy_description'})
-        if not vacancy_details:
-            vacancy_details = soup.find('div', class_='g-user-content')
-        return Vacancy.clearify(vacancy_details)
